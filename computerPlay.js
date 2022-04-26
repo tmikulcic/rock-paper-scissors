aa9d329cff8f7e0684f349c01fd6d539ff02ef79
Tomislav <tom.mikulcic@gmail.com>
2022-04-26 21:10:05 +0200

    Add .

--- computerPlay.js
+++ computerPlay.js
@@ -1,5 +1,7 @@
 let computer = ['rock', 'paper', 'scissors'];
 let player = '';
+let playerScore = 0;
+let cpuScore = 0;
 
 function ComputerPlay() {
   return computer[Math.floor(Math.random() * computer.length)];
@@ -22,20 +24,34 @@ function Scissors() {
   Round(player, computer[Math.floor(Math.random() * computer.length)]);
 }
 
+function Victory() { }
+
+function Defeat(){}
+
 function Round(player, computer) {
-  console.log(player);
-  console.log(computer);
+  console.log(player + ' ' + playerScore + ' ' + computer + ' ' + cpuScore);
+  if (playerScore === 5) console.log('Victory');
+  else if (cpuScore === 5) console.log('Defeat');
+
   if (
     (player === 'paper' && computer === 'rock') ||
     (player === 'rock' && computer === 'scissors') ||
     (player === 'scissors' && computer === 'paper')
-  )
+  ) {
+    playerScore++;
     return console.log('You win');
+  }
+
   else if (
     (player === 'rock' && computer === 'paper') ||
     (player === 'scissors' && computer === 'rock') ||
     (player === 'paper' && computer === 'scissors')
-  )
+  ) {
+    cpuScore++;
     return console.log('You lose');
+  }
+    
   else console.log(`It's a tie`);
+
+ 
 }
